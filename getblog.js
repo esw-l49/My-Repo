e2aedd06ce78d867f11427f57d8b09210f48415a
sgmw <sgmw@sgmwdeMacBook-Pro.local>
2020-07-22 17:06:03 +0800

    add blog style

--- getblog.js
+++ getblog.js
@@ -1,26 +1,66 @@
 const content = document.querySelector(".blog-content");
+const prev = document.querySelector("#prev"); // previous page
+const next = document.querySelector("#next"); // next page
+const postsPage = document.querySelector(".page"); // total pages
+const postTotal = 100;
+let id = 1;
+postsPage.innerHTML = id;
+window.onload = () => getPost(id);
 
-// method:get or post;
-const json = {
-  name: "jing",
-  age: 2,
-};
-console.log(JSON.stringify(json));
+prev.addEventListener("click", () => {
+  id = id > 1 ? id - 1 : postTotal;
+  postsPage.innerHTML = id;
+  getPost(id);
+});
+next.addEventListener("click", () => {
+  id = id < 100 ? id + 1 : 1;
+  postsPage.innerHTML = id;
+  getPost(id);
+});
 
-fetch("http://jsonplaceholder.typicode.com/posts", { method: "get" })
-  .then((res) => res.json())
-  .then((res) => {
-    const posts = res;
-    for (let post of posts) {
+function getPost(id) {
+  fetch("http://jsonplaceholder.typicode.com/posts/" + id, { method: "get" })
+    .then((res) => res.json())
+    .then((res) => {
+      const post = res;
+      const img = document.createElement("img");
       const title = document.createElement("h3");
       const userID = document.createElement("h5");
-      const bodyCOntent = document.createElement("p");
+      const bodyContent = document.createElement("p");
+      const postContent = document.createElement("div");
+      const postContainer = document.createElement("div");
+      postContainer.classList.add("post-container");
+      img.src =
+        "https://uploadbeta.com/api/pictures/random/?key=BingEverydayWallpaperPicture";
+      img.alt = "no picture";
+      img.classList.add("img-thumbnail");
       title.innerHTML = post.title;
-      bodyCOntent.innerHTML = post.body;
-      userID.innerHTML = "<a>用户ID：" + post.userId + "</a>";
-      content.appendChild(title);
-      content.appendChild(userID);
-      content.appendChild(bodyCOntent);
-    }
-  })
-  .catch((error) => console.error(error));
+      bodyContent.innerHTML = post.body;
+      userID.innerHTML = "<a>User ID：" + post.userId + "</a>";
+
+      // 渲染之前先清空
+      content.innerHTML = "";
+      postContent.appendChild(title);
+      postContent.appendChild(userID);
+      postContent.appendChild(bodyContent);
+      postContainer.appendChild(img);
+      postContainer.appendChild(postContent);
+      content.appendChild(postContainer);
+    })
+    .catch((error) => console.error(error));
+}
+function $(param) {
+  // #param   class: .param
+  return document.querySelector("." + param)
+    ? document.querySelector("." + param)
+    : document.querySelector("#" + param);
+}
+
+const obj = new Object({ name: "Jhon", age: 23, city: "Beijing" });
+for (let item in obj) {
+  console.log(obj[item]);
+}
+
+function ObjDefin(prop) {
+  return { prop };
+}
